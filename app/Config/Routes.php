4872308bd20e6409aee3d0063f6307df76b8c96e
rahmaninsani <rahmaninsani54@gmail.com>
2021-07-21 02:44:57 +0700

    Penambahan konten Pemesanan

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -35,6 +35,7 @@ $routes->setAutoRoute(true);
 $routes->get('/', 'Beranda::index');
 $routes->get('/login', 'Login::index');
 $routes->get('/menu', 'Menu::index');
+$routes->get('/pemesanan', 'Pemesanan::index');
 
 /*
  * --------------------------------------------------------------------
