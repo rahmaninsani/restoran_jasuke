71a5c815e1125c02652a3a500952631e5dd0f32c
Ilman Aulya <ilman.10119177@mahasiswa.unikom.ac.id>
2021-07-22 08:54:05 +0700

    update index.php
    penyesuaian tampilan pelaporan

--- app/Views/laporan/index.php
+++ app/Views/laporan/index.php
@@ -22,6 +22,7 @@
                 <div class="col justify-content-center">
                     <!-- select -->
                     <div class="form-group p-3">
+                        <label>Jangka Pelaporan</label>
                         <select class="form-control">
                             <option>Harian</option>
                             <option>Mingguan</option>
@@ -69,7 +70,7 @@
                                 <div class="inner">
                                     <p class="mb-1">Total Data Pembayaran</p>
                                     <h3 class="row justify-content-center">Rp. 30.000.000 ,-</h3>
-                                    <sub class="row justify-content-center mb-0" style="font-size: 20px"></sub>
+                                    <sub class="row justify-content-center mb-" style="font-size: 20px"></sub>
                                 </div>
                                 <div class="icon">
                                     <i class="fas fa-money-bill-wave-alt"></i></i>
