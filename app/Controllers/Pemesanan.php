4e820fed01d90758137e7e155b4ecf8e06f98c81
mjafarshidik <mjafarshidik86@gmail.com>
2021-07-24 08:30:49 +0700

    Penambahan fitur pencarian meja, tambah pesanan, dan update status pesanan
    Penambahan fitur pencarian meja, tambah pesanan, dan update status pesanan.

--- app/Controllers/Pemesanan.php
+++ app/Controllers/Pemesanan.php
@@ -1,8 +1,9 @@
-<?php 
+<?php
 
 namespace App\Controllers;
 
-class Pemesanan extends BaseController {
+class Pemesanan extends BaseController
+{
 
   public function index()
   {
@@ -13,7 +14,30 @@ class Pemesanan extends BaseController {
     return view('pemesanan/index', $data);
   }
 
+  public function pencarian_meja()
+  {
+    $data = [
+      'title' => 'Pencarian Meja',
+    ];
+
+    return view('pemesanan/pencarian_meja', $data);
+  }
+
+  public function tambah_pesanan()
+  {
+    $data = [
+      'title' => 'Tambah Pesanan',
+    ];
+
+    return view('pemesanan/tambah_pesanan', $data);
+  }
+
+  public function ubah_pesanan()
+  {
+    $data = [
+      'title' => 'Ubah Pesanan',
+    ];
+
+    return view('pemesanan/ubah_pesanan', $data);
+  }
 }
-
-
-?>
