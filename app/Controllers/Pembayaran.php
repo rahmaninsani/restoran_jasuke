5e35a9fed94c61af783f6324aa0958d1f582c8e2
Ilman Aulya <ilman.10119177@mahasiswa.unikom.ac.id>
2021-08-02 16:24:37 +0700

    memperbaiki tampilan dan lojik yang berantakan pasca merge

--- app/Controllers/Pembayaran.php
+++ app/Controllers/Pembayaran.php
@@ -4,10 +4,6 @@ namespace App\Controllers;
 
 class Pembayaran extends BaseController
 {
-<<<<<<< HEAD
-
-=======
->>>>>>> 74688028d8cdc6c3eb173e99de05e7cb01607b78
   public function index()
   {
     $data = [
