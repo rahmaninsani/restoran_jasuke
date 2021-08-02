6632757e4c344225289f3db53e91138eb942c3e7
rahmaninsani <rahmaninsani54@gmail.com>
2021-08-02 09:01:28 +0700

    perbaikan conflit menu & file sql

--- app/Views/menu/index.php
+++ app/Views/menu/index.php
@@ -1,10 +1,6 @@
 <?= $this->extend('layout/template'); ?>
 
 <?= $this->section('content'); ?>
-<<<<<<< HEAD
-=======
-
->>>>>>> 74688028d8cdc6c3eb173e99de05e7cb01607b78
 <div class="wrapper">
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
@@ -13,13 +9,8 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-<<<<<<< HEAD
           <h1 class="mt-2">Daftar Menu</h1>
             <a href="/menu/create" class="btn btn-primary mt-3">Tambah Data Menu</a>
-=======
-            <a href="#" class="btn btn-success">Tambah Menu</a>
-            <h1 class="mt-4">Daftar Menu</h1>
->>>>>>> 74688028d8cdc6c3eb173e99de05e7cb01607b78
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
@@ -32,7 +23,6 @@
     </div>
     <!-- /.content-header -->
 
-<<<<<<< HEAD
     <div class="container">
       <div class="row">
         <div class="col-6">
@@ -89,122 +79,4 @@
 </div>
 
 
-=======
-    <!-- Main content -->
-    <section class="content">
-      <div class="container-fluid">
-        <div class="card card-success">
-          <div class="card-body">
-            <div class="row">
-              <div class="col-md-12 col-lg-6 col-xl-4">
-                <div class="card mb-2 bg-gradient-dark">
-                  <img class="card-img-top" src="/assets/img/telur-dadar.jpg" alt="Dist Photo 1">
-                  <div class="card-img-overlay d-flex flex-column justify-content-end">
-                    <h5 class="card-title text-primary text-white"><b>Nasi Goreng</b></h5>
-                    <!-- <p class="card-text text-white pb-2 pt-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit sed do eiusmod tempor.</p> -->
-                    <p class="text-white">Rp13000</p>
-                    <p class="text-white">12 porsi</p>
-                  </div>
-                </div>
-                <div class="text-center">
-                  <button class="btn btn-primary">Ubah</button>
-                  <button class="btn btn-danger">Hapus</button>
-                </div>
-              </div>
-              <div class="col-md-12 col-lg-6 col-xl-4">
-                <div class="card mb-2 bg-gradient-dark">
-                  <img class="card-img-top" src="/assets/img/telur-dadar.jpg" alt="Dist Photo 1">
-                  <div class="card-img-overlay d-flex flex-column justify-content-end">
-                    <h5 class="card-title text-primary text-white"><b>Nasi Goreng</b></h5>
-                    <!-- <p class="card-text text-white pb-2 pt-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit sed do eiusmod tempor.</p> -->
-                    <p class="text-white">Rp13000</p>
-                    <p class="text-white">12 porsi</p>
-                  </div>
-                </div>
-                <div class="text-center">
-                  <button class="btn btn-primary">Ubah</button>
-                  <button class="btn btn-danger">Hapus</button>
-                </div>
-              </div>
-              <div class="col-md-12 col-lg-6 col-xl-4">
-                <div class="card mb-2 bg-gradient-dark">
-                  <img class="card-img-top" src="/assets/img/telur-dadar.jpg" alt="Dist Photo 1">
-                  <div class="card-img-overlay d-flex flex-column justify-content-end">
-                    <h5 class="card-title text-primary text-white"><b>Nasi Goreng</b></h5>
-                    <!-- <p class="card-text text-white pb-2 pt-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit sed do eiusmod tempor.</p> -->
-                    <p class="text-white">Rp13000</p>
-                    <p class="text-white">12 porsi</p>
-                  </div>
-                </div>
-                <div class="text-center">
-                  <button class="btn btn-primary">Ubah</button>
-                  <button class="btn btn-danger">Hapus</button>
-                </div>
-              </div>
-              
-            </div>
-          </div>
-        </div>
-      </div><!-- /.container-fluid -->
-      <div class="container-fluid">
-        <div class="card card-success">
-          <div class="card-body">
-            <div class="row">
-              <div class="col-md-12 col-lg-6 col-xl-4">
-                <div class="card mb-2 bg-gradient-dark">
-                  <img class="card-img-top" src="/assets/img/telur-dadar.jpg" alt="Dist Photo 1">
-                  <div class="card-img-overlay d-flex flex-column justify-content-end">
-                    <h5 class="card-title text-primary text-white"><b>Nasi Goreng</b></h5>
-                    <!-- <p class="card-text text-white pb-2 pt-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit sed do eiusmod tempor.</p> -->
-                    <p class="text-white">Rp13000</p>
-                    <p class="text-white">12 porsi</p>
-                  </div>
-                </div>
-                <div class="text-center">
-                  <button class="btn btn-primary">Ubah</button>
-                  <button class="btn btn-danger">Hapus</button>
-                </div>
-              </div>
-              <div class="col-md-12 col-lg-6 col-xl-4">
-                <div class="card mb-2 bg-gradient-dark">
-                  <img class="card-img-top" src="/assets/img/telur-dadar.jpg" alt="Dist Photo 1">
-                  <div class="card-img-overlay d-flex flex-column justify-content-end">
-                    <h5 class="card-title text-primary text-white"><b>Nasi Goreng</b></h5>
-                    <!-- <p class="card-text text-white pb-2 pt-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit sed do eiusmod tempor.</p> -->
-                    <p class="text-white">Rp13000</p>
-                    <p class="text-white">12 porsi</p>
-                  </div>
-                </div>
-                <div class="text-center">
-                  <button class="btn btn-primary">Ubah</button>
-                  <button class="btn btn-danger">Hapus</button>
-                </div>
-              </div>
-              <div class="col-md-12 col-lg-6 col-xl-4">
-                <div class="card mb-2 bg-gradient-dark">
-                  <img class="card-img-top" src="/assets/img/telur-dadar.jpg" alt="Dist Photo 1">
-                  <div class="card-img-overlay d-flex flex-column justify-content-end">
-                    <h5 class="card-title text-primary text-white"><b>Nasi Goreng</b></h5>
-                    <!-- <p class="card-text text-white pb-2 pt-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit sed do eiusmod tempor.</p> -->
-                    <p class="text-white">Rp13000</p>
-                    <p class="text-white">12 porsi</p>
-                  </div>
-                </div>
-                <div class="text-center">
-                  <button class="btn btn-primary">Ubah</button>
-                  <button class="btn btn-danger">Hapus</button>
-                </div>
-              </div>
-              
-            </div>
-          </div>
-        </div>
-      </div><!-- /.container-fluid -->
-    </section>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
-</div>
-
->>>>>>> 74688028d8cdc6c3eb173e99de05e7cb01607b78
 <?= $this->endSection(); ?>
--- restoran_jasuke.sql
+++ restoran_jasuke.sql
@@ -2,17 +2,10 @@
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
-<<<<<<< HEAD
--- Host: 127.0.0.1
--- Generation Time: Aug 01, 2021 at 04:57 AM
--- Server version: 10.4.20-MariaDB
--- PHP Version: 8.0.8
-=======
 -- Host: localhost
--- Generation Time: Jul 28, 2021 at 07:28 AM
+-- Generation Time: Aug 02, 2021 at 02:00 AM
 -- Server version: 10.5.8-MariaDB-log
 -- PHP Version: 7.4.20
->>>>>>> 74688028d8cdc6c3eb173e99de05e7cb01607b78
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -27,24 +20,12 @@ SET time_zone = "+00:00";
 --
 -- Database: `restoran_jasuke`
 --
-<<<<<<< HEAD
-=======
 CREATE DATABASE IF NOT EXISTS `restoran_jasuke` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 USE `restoran_jasuke`;
->>>>>>> 74688028d8cdc6c3eb173e99de05e7cb01607b78
 
 -- --------------------------------------------------------
 
 --
-<<<<<<< HEAD
--- Table structure for table `meja`
---
-
-CREATE TABLE `meja` (
-  `no_meja` int(11) NOT NULL,
-  `status` enum('Kosong','Terisi') DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-=======
 -- Table structure for table `detail_pemesanan`
 --
 
@@ -56,9 +37,9 @@ CREATE TABLE IF NOT EXISTS `detail_pemesanan` (
   `kuantitas` int(11) DEFAULT NULL,
   `subtotal` decimal(10,2) DEFAULT NULL,
   `status_pemesanan` enum('Belum Selesai','Selesai') COLLATE utf8_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`no_pemesanan`,`kode_menu`,`no_pembayaran`),
-  KEY `kode_menu` (`kode_menu`),
-  KEY `no_pembayaran` (`no_pembayaran`)
+  KEY `detail_pemesanan_ibfk_1` (`no_pemesanan`),
+  KEY `detail_pemesanan_ibfk_3` (`no_pembayaran`),
+  KEY `detail_pemesanan_ibfk_2` (`kode_menu`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
@@ -84,7 +65,6 @@ CREATE TABLE IF NOT EXISTS `meja` (
   `status` enum('Kosong','Terisi') COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`no_meja`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
->>>>>>> 74688028d8cdc6c3eb173e99de05e7cb01607b78
 
 --
 -- Dumping data for table `meja`
@@ -103,34 +83,22 @@ INSERT INTO `meja` (`no_meja`, `status`) VALUES
 -- Table structure for table `menu`
 --
 
-<<<<<<< HEAD
-CREATE TABLE `menu` (
-  `kode_menu` varchar(4) NOT NULL,
-  `nama` varchar(50) DEFAULT NULL,
-  `slug` varchar(225) NOT NULL,
-  `harga` decimal(10,2) DEFAULT NULL,
-  `stok` int(11) DEFAULT NULL,
-  `gambar` varchar(50) DEFAULT NULL,
-  `deskripsi` text DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-=======
 DROP TABLE IF EXISTS `menu`;
 CREATE TABLE IF NOT EXISTS `menu` (
   `kode_menu` varchar(4) COLLATE utf8_unicode_ci NOT NULL,
   `nama` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `slug` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `harga` decimal(10,2) DEFAULT NULL,
   `stok` int(11) DEFAULT NULL,
   `gambar` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
   `deskripsi` text COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`kode_menu`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
->>>>>>> 74688028d8cdc6c3eb173e99de05e7cb01607b78
 
 --
 -- Dumping data for table `menu`
 --
 
-<<<<<<< HEAD
 INSERT INTO `menu` (`kode_menu`, `nama`, `slug`, `harga`, `stok`, `gambar`, `deskripsi`) VALUES
 ('MN01', 'Rendang', 'rendang', '15000.00', 20, '1627453710_6b72aeb6d7fa05dd6a1e.jpg', 'Rendang atau randang adalah masakan daging asli Indonesia yang berasal dari daerah Minangkabau. '),
 ('MN02', 'Lotek', 'lotek', '15000.00', 22, '1627453758_65bab4854c445476e0cf.jpg', 'Lotek adalah salah satu makanan dari Jawa Barat yang mudah ditemukan di seluruh wilayah Jawa Barat'),
@@ -142,12 +110,6 @@ INSERT INTO `menu` (`kode_menu`, `nama`, `slug`, `harga`, `stok`, `gambar`, `des
 ('MN09', 'Ayam bakar', 'ayam-bakar', '30000.00', 5, '1627659680_deb3ce68cdd1bf2abc63.jpg', 'adalah sebuah hidangan Asia Tenggara Maritim, terutama hidangan Indonesia atau Malaysia'),
 ('MN10', 'Tumpeng', 'tumpeng', '30000.00', 5, '1627743117_59bd6e71ecf8ea30e160.jpg', 'Tumpeng atau nasi tumpeng adalah makanan masyarakat Jawa yang penyajian nasinya dibentuk kerucut dan ditata bersama dengan lauk-pauknya.'),
 ('MN11', 'Ayam geprek', 'ayam-geprek', '15000.00', 5, '1627743573_fcdfeefc446f97a95ffc.jpg', 'ayam geprek terdiri dari ayam goreng dan sambel dengan rempah-rempah asli indonesia');
-=======
-INSERT INTO `menu` (`kode_menu`, `nama`, `harga`, `stok`, `gambar`, `deskripsi`) VALUES
-('MN01', 'Rendang', '13000.00', 50, 'rendang.jpg', 'Rendang Sapi cita rasa pejabat harga merakyat'),
-('MN02', 'Semur Jengkol', '5000.00', 67, 'semur-jengkol.jpg', 'Semur Jengkol sedap'),
-('MN03', 'Ayam Penyet', '8000.00', 182, 'ayam-penyet.jpg', 'Ayam Penyet nikmat');
->>>>>>> 74688028d8cdc6c3eb173e99de05e7cb01607b78
 
 -- --------------------------------------------------------
 
@@ -162,14 +124,6 @@ CREATE TABLE IF NOT EXISTS `pegawai` (
   `jabatan` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL,
   `tempat_lahir` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL,
   `tanggal_lahir` date DEFAULT NULL,
-<<<<<<< HEAD
-  `jenis_kelamin` enum('Laki-laki','Perempuan') DEFAULT NULL,
-  `alamat` varchar(100) DEFAULT NULL,
-  `no_telepon` varchar(15) DEFAULT NULL,
-  `username` varchar(20) DEFAULT NULL,
-  `password` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-=======
   `jenis_kelamin` enum('Laki-laki','Perempuan') COLLATE utf8_unicode_ci DEFAULT NULL,
   `alamat` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
   `no_telepon` varchar(15) COLLATE utf8_unicode_ci DEFAULT NULL,
@@ -177,7 +131,6 @@ CREATE TABLE IF NOT EXISTS `pegawai` (
   `password` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`nrp`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
->>>>>>> 74688028d8cdc6c3eb173e99de05e7cb01607b78
 
 --
 -- Dumping data for table `pegawai`
@@ -194,29 +147,19 @@ INSERT INTO `pegawai` (`nrp`, `nama`, `jabatan`, `tempat_lahir`, `tanggal_lahir`
 -- Table structure for table `pembayaran`
 --
 
-<<<<<<< HEAD
-CREATE TABLE `pembayaran` (
-  `no_pembayaran` int(11) NOT NULL,
-=======
 DROP TABLE IF EXISTS `pembayaran`;
 CREATE TABLE IF NOT EXISTS `pembayaran` (
   `no_pembayaran` int(11) NOT NULL AUTO_INCREMENT,
->>>>>>> 74688028d8cdc6c3eb173e99de05e7cb01607b78
   `tanggal` date DEFAULT NULL,
   `total_harga` decimal(10,2) DEFAULT NULL,
   `pajak` decimal(10,2) DEFAULT NULL,
   `total_bayar` decimal(10,2) DEFAULT NULL,
   `uang_bayar` decimal(10,2) DEFAULT NULL,
   `uang_kembalian` decimal(10,2) DEFAULT NULL,
-<<<<<<< HEAD
-  `nrp` varchar(6) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-=======
   `nrp` varchar(6) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`no_pembayaran`),
   KEY `nrp` (`nrp`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
->>>>>>> 74688028d8cdc6c3eb173e99de05e7cb01607b78
 
 --
 -- Dumping data for table `pembayaran`
@@ -232,14 +175,6 @@ INSERT INTO `pembayaran` (`no_pembayaran`, `tanggal`, `total_harga`, `pajak`, `t
 -- Table structure for table `pemesanan`
 --
 
-<<<<<<< HEAD
-CREATE TABLE `pemesanan` (
-  `no_pemesanan` int(11) NOT NULL,
-  `nama_pelanggan` varchar(50) DEFAULT NULL,
-  `tanggal` date DEFAULT NULL,
-  `no_meja` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-=======
 DROP TABLE IF EXISTS `pemesanan`;
 CREATE TABLE IF NOT EXISTS `pemesanan` (
   `no_pemesanan` int(11) NOT NULL AUTO_INCREMENT,
@@ -249,7 +184,6 @@ CREATE TABLE IF NOT EXISTS `pemesanan` (
   PRIMARY KEY (`no_pemesanan`),
   KEY `no_meja` (`no_meja`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
->>>>>>> 74688028d8cdc6c3eb173e99de05e7cb01607b78
 
 --
 -- Dumping data for table `pemesanan`
@@ -260,68 +194,6 @@ INSERT INTO `pemesanan` (`no_pemesanan`, `nama_pelanggan`, `tanggal`, `no_meja`)
 (2, 'Muhammad Jafar Shidik', '2021-07-16', 3);
 
 --
-<<<<<<< HEAD
--- Indexes for dumped tables
---
-
---
--- Indexes for table `meja`
---
-ALTER TABLE `meja`
-  ADD PRIMARY KEY (`no_meja`);
-
---
--- Indexes for table `menu`
---
-ALTER TABLE `menu`
-  ADD PRIMARY KEY (`kode_menu`);
-
---
--- Indexes for table `pegawai`
---
-ALTER TABLE `pegawai`
-  ADD PRIMARY KEY (`nrp`);
-
---
--- Indexes for table `pembayaran`
---
-ALTER TABLE `pembayaran`
-  ADD PRIMARY KEY (`no_pembayaran`),
-  ADD KEY `nrp` (`nrp`);
-
---
--- Indexes for table `pemesanan`
---
-ALTER TABLE `pemesanan`
-  ADD PRIMARY KEY (`no_pemesanan`),
-  ADD KEY `no_meja` (`no_meja`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `meja`
---
-ALTER TABLE `meja`
-  MODIFY `no_meja` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT for table `pembayaran`
---
-ALTER TABLE `pembayaran`
-  MODIFY `no_pembayaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `pemesanan`
---
-ALTER TABLE `pemesanan`
-  MODIFY `no_pemesanan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- Constraints for dumped tables
---
-=======
 -- Constraints for dumped tables
 --
 
@@ -330,21 +202,20 @@ ALTER TABLE `pemesanan`
 --
 ALTER TABLE `detail_pemesanan`
   ADD CONSTRAINT `detail_pemesanan_ibfk_1` FOREIGN KEY (`no_pemesanan`) REFERENCES `pemesanan` (`no_pemesanan`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `detail_pemesanan_ibfk_2` FOREIGN KEY (`kode_menu`) REFERENCES `menu` (`kode_menu`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `detail_pemesanan_ibfk_2` FOREIGN KEY (`kode_menu`) REFERENCES `menu` (`kode_menu`) ON DELETE NO ACTION ON UPDATE CASCADE,
   ADD CONSTRAINT `detail_pemesanan_ibfk_3` FOREIGN KEY (`no_pembayaran`) REFERENCES `pembayaran` (`no_pembayaran`) ON DELETE CASCADE ON UPDATE CASCADE;
->>>>>>> 74688028d8cdc6c3eb173e99de05e7cb01607b78
 
 --
 -- Constraints for table `pembayaran`
 --
 ALTER TABLE `pembayaran`
-  ADD CONSTRAINT `pembayaran_ibfk_1` FOREIGN KEY (`nrp`) REFERENCES `pegawai` (`nrp`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `pembayaran_ibfk_1` FOREIGN KEY (`nrp`) REFERENCES `pegawai` (`nrp`) ON DELETE NO ACTION ON UPDATE CASCADE;
 
 --
 -- Constraints for table `pemesanan`
 --
 ALTER TABLE `pemesanan`
-  ADD CONSTRAINT `pemesanan_ibfk_1` FOREIGN KEY (`no_meja`) REFERENCES `meja` (`no_meja`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `pemesanan_ibfk_1` FOREIGN KEY (`no_meja`) REFERENCES `meja` (`no_meja`) ON DELETE NO ACTION ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
