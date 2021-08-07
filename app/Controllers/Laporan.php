8f82eb26bf4b6f232e163c18c2dec672e4ee2413
Ilman Aulya <ilman.10119177@mahasiswa.unikom.ac.id>
2021-08-07 09:28:48 +0700

    Revert "coba controller"
    This reverts commit 3bcf1ad552c3b79a24d64d4c6bbb0972f93af898.

--- app/Controllers/Laporan.php
+++ app/Controllers/Laporan.php
@@ -1,32 +1,16 @@
-<?php
+<?php 
 
 namespace App\Controllers;
 
-use App\Models\PemesananModel;
-use App\Models\PembayaranModel;
-
-class Laporan extends BaseController
-{
-  protected
-    $pemesananModel,
-    $pembayaranModel;
-
-  public function __construct()
-  {
-    $this->pemesananModel = new PemesananModel();
-    $this->pembayaranModel = new PembayaranModel();
-  }
+class Laporan extends BaseController {
 
   public function index()
   {
-    $pemesanan = $this->pemesananModel->getPemesanan();
-    $pembayaran = $this->pembayaranModel->getPembayaran();
     $data = [
       'title' => 'Pelaporan',
-      'pemesanan' => $pemesanan,
-      'pembayaran' => $pembayaran,
     ];
 
     return view('laporan/index', $data);
   }
+
 }
--- app/Models/PembayaranModel.php
+++ app/Models/PembayaranModel.php
@@ -6,55 +6,51 @@ use CodeIgniter\Model;
 
 class PembayaranModel extends Model
 {
-  protected $table = 'pembayaran';
-  protected $primaryKey = 'no_pembayaran';
-  protected $allowedFields = ['tanggal_pembayaran', 'total_harga', 'pajak', 'total_bayar', 'uang_bayar', 'uang_kembalian', 'status_pembayaran', 'nrp'];
+    protected $table = 'pembayaran';
+    protected $primaryKey = 'no_pembayaran';
+    protected $allowedFields = ['tanggal_pembayaran', 'total_harga', 'pajak', 'total_bayar', 'uang_bayar', 'uang_kembalian', 'status_pembayaran', 'nrp'];
 
-  // Insert pembayaran
-  public function savePembayaran($tanggal)
-  {
-    $data = [
-      'tanggal_pembayaran' => $tanggal,
-      'nrp' => session()->get('nrp'),
-    ];
-    return $this->save($data);
-  }
-
-  // Update pembayaran
-  public function updatePembayaran($no_pembayaran, $total_harga, $pajak, $total_bayar, $tanggal = false)
-  {
-    $data = [  //update makanya pake no_pembayaran
-      'no_pembayaran' => $no_pembayaran,
-      'total_harga' => $total_harga,
-      'pajak' => $pajak,
-      'total_bayar' => $total_bayar,
-      'nrp' => session()->get('nrp'),
-    ];
-
-    if ($tanggal != false) {
-      $data['tanggal_pembayaran'] = $tanggal;
+    // Insert pembayaran
+    public function savePembayaran($tanggal) 
+    {
+      $data = [
+        'tanggal_pembayaran' => $tanggal,
+        'nrp' => session()->get('nrp'),
+      ];
+      return $this->save($data);
     }
 
-    return $this->save($data);
-  }
+    // Update pembayaran
+    public function updatePembayaran($no_pembayaran, $total_harga, $pajak, $total_bayar, $tanggal = false) 
+    {
+      $data = [  //update makanya pake no_pembayaran
+        'no_pembayaran' => $no_pembayaran,
+        'total_harga' => $total_harga,
+        'pajak' => $pajak,
+        'total_bayar' => $total_bayar,
+        'nrp' => session()->get('nrp'),
+      ];
 
-  // Delete Pembayaran
-  public function deletePembayaran($no_pembayaran)
-  {
-    return $this->delete($no_pembayaran);
-  }
+      if($tanggal != false) {
+        $data['tanggal_pembayaran'] = $tanggal;
+      }
 
-  public function getPemasukanHarian()
-  {
-    $today = date("Y-m-d");
-    $builder = $this->selectSum('total_bayar');
-    $query = $builder->getWhere(['tanggal_pembayaran' => $today])->getResultArray()[0]['total_bayar'];
+      return $this->save($data);
+      
+    }
 
-    return $query;
-  }
+    // Delete Pembayaran
+    public function deletePembayaran($no_pembayaran) 
+    {
+      return $this->delete($no_pembayaran);
+    }
 
-  public function getTotalPembayaran()
-  {
-    $builder = $this->selectSum('total_harga');
-  }
+    public function getPemasukanHarian() 
+    { 
+      $today = date("Y-m-d");
+      $builder =$this->selectSum('total_bayar');
+      $query = $builder->getWhere(['tanggal_pembayaran' => $today])->getResultArray()[0]['total_bayar'];
+
+      return $query;
+    }
 }
